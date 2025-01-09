238b1075d3c58e7588e80321a6a5e246008b45f1
prajakta02-apk <prajaktasawarkar02@gmail.com>
2025-01-09 23:02:02 +0530

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -1,1 +1,14 @@
-
+CREATE TABLE Employee
+(
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName  VARCHAR(50),
+    DateOfBirth DATE,
+    Gender  CHAR(1),
+    HireDate DATE,
+    JobTitle  VARCHAR(200),
+    Department VARCHAR(200),
+    Salary  DECIMAL(10,2),
+    EmailID VARCHAR(100),
+    PhoneNumber VARCHAR(15)
+);
